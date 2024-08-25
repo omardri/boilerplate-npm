eba6e61c544852cbbbba5450c6c3c8196433eb14
Omar Douhri <omardouhri5@gmail.com>
2024-08-25 20:15:03 +0000

    print Hello World

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (!process.env.DISABLE_XORIGIN) {
     var origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
          console.log(origin);
-         console.log("Hello World!");
+         console.log("Hello World");
          res.setHeader('Access-Control-Allow-Origin', origin);
          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
